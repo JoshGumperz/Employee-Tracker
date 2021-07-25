2ebb629b27d3477e0c4789e21828fbb8096cf905
Josh Gumperz <joshg3023@gmail.com>
2021-07-25 13:23:41 -0700

    installed required libraries, added questions to questions.js, and added gitignore

---
+++ questions/questions.js
@@ -0,0 +1,67 @@
+[
+    {
+        type: "list",
+        message: "What would you like to do",
+        name: "initialQuestion",
+        choices: ["View All Employees", "Add Employees", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
+    }
+]
+[
+    {
+        type: "Input",
+        message: "Enter New Department Name:",
+        name: "departmentName",
+    }
+]
+[
+    {
+        type: "Input",
+        message: "Enter New Role Name:",
+        name: "roleName",
+    }
+]
+[
+    {
+        type: "Input",
+        message: "Enter New Role Salary",
+        name: "roleSalary",
+    }
+]
+[
+    {
+        type: "list",
+        message: "Enter New Role's Department:",
+        name: "roleDepartment",
+        choices: []
+    }
+]
+[
+    {
+        type: "Input",
+        message: "Enter New Employee First Name:",
+        name: "employeeFirstName",
+    }
+]
+[
+    {
+        type: "Input",
+        message: "Enter New Employee Last Name:",
+        name: "employeeLasttName",
+    }
+]
+[
+    {
+        type: "list",
+        message: "Enter New Employee's Role:",
+        name: "employeeRole",
+        choices: []
+    }
+]
+[
+    {
+        type: "list",
+        message: "Enter New Employee's Manager:",
+        name: "employeeManager",
+        choices: []
+    }
+]
